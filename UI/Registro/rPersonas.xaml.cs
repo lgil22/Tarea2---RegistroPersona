97ccdec591ac74e014ffb041e80fe0bccb4d4d17
Luis David Gil Baez <luis_gil@ucne.edu.do>
2020-01-27 18:14:29 -0400

    Escribiendo Etiquetas

--- UI/Registro/rPersonas.xaml.cs
+++ UI/Registro/rPersonas.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,24 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace RegistroPersona
+namespace RegistroPersona.UI.Registro
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for rPersonas.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class rPersonas : Window
     {
-        public MainWindow()
+        public rPersonas()
         {
             InitializeComponent();
-        }
-
-        private void MenuItem_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
